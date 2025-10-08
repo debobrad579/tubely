8c0db3656aa5eaa7e34b0b75258c0e950df22e94
Brady DeBoer <bradydeboer195@gmail.com>
2025-10-07 22:45:23 -0400

    file server

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +35,54 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Failed to form file", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Failed to get video", err)
+		return
+	}
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized", nil)
+		return
+	}
+
+	mediaType, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Failed to parse media type", err)
+		return
+	}
+	if mediaType != "image/jpeg" && mediaType != "image/png" {
+		respondWithError(w, http.StatusBadRequest, "Incorrect file format", nil)
+		return
+	}
+
+	fileExtention := strings.TrimPrefix(mediaType, "image/")
+	b := make([]byte, 32)
+	rand.Read(b)
+	filename := base64.RawURLEncoding.EncodeToString(b)
+
+	filePath := filepath.Join(cfg.assetsRoot, fmt.Sprintf("%s.%s", filename, fileExtention))
+	newFile, err := os.Create(filePath)
+	io.Copy(newFile, file)
+
+	thumbnailURL := fmt.Sprintf("http://localhost:8091/assets/%s.%s", filename, fileExtention)
+
+	video.ThumbnailURL = &thumbnailURL
+
+	if err := cfg.db.UpdateVideo(video); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to update video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
